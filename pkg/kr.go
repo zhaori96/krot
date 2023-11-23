f4808a22239893c186f1b34de24c447353b660f8
zhaori96 <nleonascimento96@gmail.com>
2023-11-23 20:59:06 -0300

    defining Rotator interface

--- pkg/kr.go
+++ pkg/kr.go
@@ -1,1 +1,35 @@
 package kr
+
+import (
+	"context"
+	"time"
+)
+
+type RotatorHook func(rotator Rotator, activeKeyIds []*Key)
+
+type RotatorState uint
+
+const (
+	RotatorStateIdle RotatorState = iota
+	RotatorStateRotating
+)
+
+type RotatorStatus uint
+
+const (
+	RotatorStatusInactive RotatorStatus = iota
+	RotatorStatusActive
+)
+
+type Rotator interface {
+	Status() RotatorStatus
+	State() RotatorState
+	RotationInterval() time.Duration
+	Start() error
+	Stop()
+	Rotate(context context.Context) error
+	GetKey(context context.Context) (string, any, error)
+	GetKeyID(context context.Context) (string, error)
+	GetKeyByID(context context.Context, id string) (any, error)
+	WhenRotate(hooks ...RotatorHook)
+}
